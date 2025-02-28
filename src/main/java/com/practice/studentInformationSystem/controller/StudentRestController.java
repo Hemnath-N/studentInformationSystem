5b6adf1ee74220e30e5d20b600d4c34f959135bf
Hemnath <hemnathcn@gmail.com>
2025-02-28 11:54:04 +0530

    added data sql file

--- src/main/java/com/practice/studentInformationSystem/controller/StudentRestController.java
+++ src/main/java/com/practice/studentInformationSystem/controller/StudentRestController.java
@@ -4,6 +4,7 @@ import com.practice.studentInformationSystem.domain.StudentRequest;
 import com.practice.studentInformationSystem.domain.StudentResponse;
 import com.practice.studentInformationSystem.service.StudentInformationProcessorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,23 +14,23 @@ public class StudentRestController {
 
     private final StudentInformationProcessorService studentInformationProcessorService;
 
-    @GetMapping
+    @GetMapping("/{id}")
     public ResponseEntity<StudentResponse> getStudentInformation(@PathVariable Integer id) {
-        return ResponseEntity.ok(studentInformationProcessorService.getStudentById(id));
+        return ResponseEntity.status(HttpStatus.FOUND).body(studentInformationProcessorService.getStudentById(id));
     }
 
     @PutMapping
     public ResponseEntity<String> updateStudentInformation(@RequestBody StudentRequest studentRequest) {
-        return ResponseEntity.ok(studentInformationProcessorService.updateStudentById(studentRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(studentInformationProcessorService.updateStudentById(studentRequest));
     }
 
     @PostMapping
     public ResponseEntity<String> addStudentInformation(@RequestBody StudentRequest studentRequest) {
-        return ResponseEntity.ok(studentInformationProcessorService.addStudent(studentRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(studentInformationProcessorService.addStudent(studentRequest));
     }
 
     @DeleteMapping
     public ResponseEntity<String> deleteStudentInformation(@RequestParam Integer id) {
-        return ResponseEntity.ok(studentInformationProcessorService.deleteStudentById(id));
+        return ResponseEntity.status(HttpStatus.FOUND).body(studentInformationProcessorService.deleteStudentById(id));
     }
 }
